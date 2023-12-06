fe8e19c77d3bf6063929f570154a7d84947aa126
sajesh <sajeshp454@gmai.com>
2023-12-06 12:18:13 +0530

    terraform drtails

--- main.tf
+++ main.tf
@@ -3,7 +3,15 @@
   #  key2= value2
 #
 #    }
-provider "aws" {
 
-region = "us-east-1"
+# below creating resourcce of type local file, and
+# we have identified with particular name devops , its just a naming convection
+
+resource "local_file" "devops" {
+filename = "C:/Users/Sajesh/Desktop/AWSlearning/Git learning/teraform/teraformcode/demo.txt"
+content = "demo lecture on terraform plan and apply"
+}
+
+output "mylocalfile" {
+  value = local_file.devops.content
 }
