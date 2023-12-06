0144050ff015308e6a342be0179a5651a077b045
sajesh <sajeshp454@gmai.com>
2023-12-06 21:58:44 +0530

    updated 12 dec 10 pm

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@
 # below creating resourcce of type local file, and
 # we have identified with particular name devops , its just a naming convection
 
+
 resource "local_file" "devops" {
 filename = "C:/Users/Sajesh/Desktop/AWSlearning/Git learning/teraform/teraformcode/demo.txt"
-content = "demo lecture on terraform plan and apply"
+content = var.custom_content
 }
 
 output "mylocalfile" {
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable custom_content{
+  type = string
+ default = "pass any variable , not ask for any  value"
+
+}
